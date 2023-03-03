0ec40fb9eaa248f90555f62c7fe3037ff805284f
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-03-03 15:21:13 -0700

    New vite config

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'src/index.html'), 
-                nested: 'src/bundle/index.html'
+                nested: resolve(__dirname, 'src/showcase.html'),
             },
         },
         outDir: '../dist',
