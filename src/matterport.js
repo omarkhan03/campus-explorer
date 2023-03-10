f0248a6ddd482f19463429f93c26ccd770c2f2f4
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-03-09 23:47:11 -0700

    matterport.js listens to "connected"

--- src/matterport.js
+++ src/matterport.js
@@ -7,47 +7,44 @@ window.addEventListener('keydown', e=> {
     }
 })
 
-let mpSdk;
-showcase.addEventListener('load', async function() {
-    try {
-        mpSdk = await showcaseWindow.MP_SDK.connect(showcaseWindow);
-    }
-    catch(e) {
-        console.error(e);
-        return;
-    }
-
-    onShowcaseConnect(mpSdk);
-    console.log('Hello Bundle SDK', mpSdk);
+var targetObj = {};
+var targetProxy = new Proxy(targetObj, {
+  set: function (target, key, value) {
+      console.log(`${key} set to ${value}`);
+      target[key] = value;
+      return true;
+  }
 });
 
-async function onShowcaseConnect(mpSdk) {
-
-    await mpSdk.App.state.waitUntil((appState) => appState.phase == 'appphase.playing');
-    mpSdk.Room.data.subscribe({
-    onAdded: async function(index, item, collection) {
-        console.log (item.center);
-        console.log(item.size)
-        const source = await Promise.all([
-        mpSdk.Sensor.createSource(mpSdk.Sensor.SourceType.SPHERE, {
-            origin: { x: -9, y: 1, z: -1},
-            radius: 2,
-            userData: {
-            id: 'Safety Equipment',
-            },
-        }),
-        mpSdk.Sensor.createSource(mpSdk.Sensor.SourceType.BOX, {
-            center: { x: -10, y: 1, z: -1},
-            size: { x: 13, y: 5, z: 6 },
-            userData: {
-            id: 'Elevators Area',
-            },
+function activate() {
+    if(!connected) {
+       window.setTimeout(activate, 100); /* this checks the flag every 100 milliseconds*/
+    } else {
+        mpSdk.Room.data.subscribe({
+        onAdded: async function(index, item, collection) {
+            console.log (item.center);
+            console.log(item.size)
+            const source = await Promise.all([
+            mpSdk.Sensor.createSource(mpSdk.Sensor.SourceType.SPHERE, {
+                origin: { x: -9, y: 1, z: -1},
+                radius: 2,
+                userData: {
+                id: 'Safety Equipment',
+                },
+            }),
+            mpSdk.Sensor.createSource(mpSdk.Sensor.SourceType.BOX, {
+                center: { x: -10, y: 1, z: -1},
+                size: { x: 13, y: 5, z: 6 },
+                userData: {
+                id: 'Elevators Area',
+                },
+            })
+            ]);
+            const sensor = await mpSdk.Sensor.createSensor(mpSdk.Sensor.SensorType.CAMERA);
+            sensor.addSource(...source);
+            sensor.showDebug(true);
+            }
         })
-        ]);
-        const sensor = await mpSdk.Sensor.createSensor(mpSdk.Sensor.SensorType.CAMERA);
-        sensor.addSource(...source);
-        sensor.showDebug(true);
-        }
-    })
-
+    }
 }
+activate()
