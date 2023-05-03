ef901aefd05845ba0909aa441f2b755e5551ef0b
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-05-02 22:00:29 -0600

    segmented model based on official floorplans

--- campus-guide (unity)/Assets/Scripts/AreaDetector.cs
+++ campus-guide (unity)/Assets/Scripts/AreaDetector.cs
@@ -8,19 +8,38 @@ public class AreaDetector : MonoBehaviour
 {   
     public Text floorIndicator; 
     public Text areaIndicator; 
-    public Collider floor1;
-    public Collider floor2;
-    public Collider elevators1;
-    public Collider elevators2;
-    public Collider coffeeShop;
-    public Collider entrance;
-
-    public GameObject player;
-
     public Image elevatorOptionsBG;
     public Text elevatorOptionsHeader;
     public Text elevatorOptions;
 
+    public GameObject player;
+
+    public Collider floor1;
+    public Collider floor2;
+    public Collider elevators1;
+    public Collider elevators2;
+    // public Collider coffeeShop; // rm 131
+    // public Collider entrance;
+    // public Collider rm134;
+    // public Collider pastaShop; // rm 135
+    // public Collider rm136C;
+    // public Collider corridor;
+    // public Collider rm117A;
+
+    // Dictionary<Collider, String> areas = new Dictionary<Collider, String> {
+    //     {floor1, "Floor 1"},
+    //     {floor2, "Floor 2"},
+    //     // {elevators1, "Elevators area"},
+    //     // {elevators2, "Elevators area"},
+    //     {coffeeShop, "Coffee Shop (rm 131)"},
+    //     {entrance, "Entrance doors"},
+    //     {rm134, "rm 134"},
+    //     {pastaShop, "Pasta Shop (rm 135)"},
+    //     {rm136C, "rm 136C"},
+    //     {rm102Z, "rm 102Z"},
+    //     {rm117A, "rm 117A"}
+    // };
+    
     Collider inArea;
 
     void Start() {
@@ -34,27 +53,58 @@ public class AreaDetector : MonoBehaviour
             activateElevators(1);
         } else if (other == elevators2) {
             activateElevators(2);
-        }
 
-        if (other == entrance) {
-            Debug.Log("Player entered entrance area");
-            inArea = entrance;
-            areaIndicator.text = "Current area: Entrance doors";
-        }
-
-        if (other == coffeeShop) {
-            Debug.Log("Player entered coffee area");
-            inArea = coffeeShop;
-            areaIndicator.text = "Current area: Coffee Shop";
-        }
-
-        if (other == floor1) {
+        } else if (other == floor1) {
             Debug.Log("Player entered floor1 area");
             floorIndicator.text = "Current floor: Floor 1";
         } else if (other == floor2) {
             Debug.Log("Player entered floor2 area");
             floorIndicator.text = "Current floor: Floor 2";
-        } 
+
+        } else {
+            Debug.Log("Player entered " + other.name + " area");
+            inArea = other;
+            areaIndicator.text = "Current area: " + other.name;
+        }
+
+
+
+        // if (other == entrance) {
+        //     Debug.Log("Player entered entrance area");
+        //     inArea = entrance;
+        //     areaIndicator.text = "Current area: Entrance doors";
+        // }
+
+        // if (other == coffeeShop) {
+        //     Debug.Log("Player entered coffee area");
+        //     inArea = coffeeShop;
+        //     areaIndicator.text = "Current area: Coffee Shop (rm 131)";
+        // }
+
+        // if (other == rm134) {
+        //     Debug.Log("Player entered area 134");
+        //     inArea = rm134;
+        //     areaIndicator.text = "Current area: rm 134";
+        // }
+
+        // if (other == pastaShop) {
+        //     Debug.Log("Player entered pasta shop area");
+        //     inArea = pastaShop;
+        //     areaIndicator.text = "Current area: Pasta Shop (rm 135)";
+        // }
+
+        // if (other == rm136C) {
+        //     Debug.Log("Player entered area 136C");
+        //     inArea = pastaShop;
+        //     areaIndicator.text = "Current area:  (rm 136C)";
+        // }
+
+
+
+
+
+        //floors
+
         
 
     }
@@ -89,7 +139,7 @@ public class AreaDetector : MonoBehaviour
             } else {
                 if (!player.GetComponent<Collider>().bounds.Intersects(inArea.GetComponent<Collider>().bounds)) {
                     Debug.Log("Player left area");
-                    areaIndicator.text = "Current area: Main area";
+                    // areaIndicator.text = "Current area: -";
                     inArea = null;
                 }
             }
@@ -116,7 +166,7 @@ public class AreaDetector : MonoBehaviour
 
     private void deactivateElevators() {
         inArea = null;
-        areaIndicator.text = "Current area: Main area";
+        // areaIndicator.text = "Current area: -";
 
         elevatorOptionsBG.enabled = false;
         elevatorOptionsHeader.enabled = false;
