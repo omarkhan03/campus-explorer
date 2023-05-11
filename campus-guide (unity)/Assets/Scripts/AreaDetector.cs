547c9bcc8ebe9a41add31e1d55b9992ed32ab81f
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-05-11 14:53:01 -0600

    added stuff + configured vr settings

--- campus-guide (unity)/Assets/Scripts/AreaDetector.cs
+++ campus-guide (unity)/Assets/Scripts/AreaDetector.cs
@@ -11,6 +11,9 @@ public class AreaDetector : MonoBehaviour
     public Image elevatorOptionsBG;
     public Text elevatorOptionsHeader;
     public Text elevatorOptions;
+    public Image guideBG;
+    public Text guideHeader;
+    public Text guideOptions;
 
     public GameObject player;
 
@@ -18,6 +21,9 @@ public class AreaDetector : MonoBehaviour
     public Collider floor2;
     public Collider elevators1;
     public Collider elevators2;
+
+    public Collider rangeOfGuide;
+
     // public Collider coffeeShop; // rm 131
     // public Collider entrance;
     // public Collider rm134;
@@ -44,14 +50,17 @@ public class AreaDetector : MonoBehaviour
 
     void Start() {
         deactivateElevators();
+        deactivateGuide();
     }
 
     void OnTriggerEnter(Collider other)
     {
 
         if (other == elevators1) {  
+            inArea = elevators1;
             activateElevators(1);
         } else if (other == elevators2) {
+            inArea = elevators2;
             activateElevators(2);
 
         } else if (other == floor1) {
@@ -61,53 +70,19 @@ public class AreaDetector : MonoBehaviour
             Debug.Log("Player entered floor2 area");
             floorIndicator.text = "Current floor: Floor 2";
 
+        } else if (other == rangeOfGuide) {
+            inArea = rangeOfGuide;
+            activateGuide();
+
         } else {
             Debug.Log("Player entered " + other.name + " area");
             inArea = other;
             areaIndicator.text = "Current area: " + other.name;
         }
-
-
-
-        // if (other == entrance) {
-        //     Debug.Log("Player entered entrance area");
-        //     inArea = entrance;
-        //     areaIndicator.text = "Current area: Entrance doors";
-        // }
-
-        // if (other == coffeeShop) {
-        //     Debug.Log("Player entered coffee area");
-        //     inArea = coffeeShop;
-        //     areaIndicator.text = "Current area: Coffee Shop (rm 131)";
-        // }
-
-        // if (other == rm134) {
-        //     Debug.Log("Player entered area 134");
-        //     inArea = rm134;
-        //     areaIndicator.text = "Current area: rm 134";
-        // }
-
-        // if (other == pastaShop) {
-        //     Debug.Log("Player entered pasta shop area");
-        //     inArea = pastaShop;
-        //     areaIndicator.text = "Current area: Pasta Shop (rm 135)";
-        // }
-
-        // if (other == rm136C) {
-        //     Debug.Log("Player entered area 136C");
-        //     inArea = pastaShop;
-        //     areaIndicator.text = "Current area:  (rm 136C)";
-        // }
-
-
-
-
-
-        //floors
-
         
 
     }
+
 
     void Update()
     {
@@ -115,12 +90,15 @@ public class AreaDetector : MonoBehaviour
             // checking to see if the player is still in the area specified by the collider
             if ( inArea != null && !player.GetComponent<Collider>().bounds.Intersects(inArea.GetComponent<Collider>().bounds)) {
                 Debug.Log("Player left area");
-                // areaIndicator.text = "Current area: -";
                 inArea = null;
             }
 
             if ( inArea != elevators1 && inArea != elevators2) {
                 deactivateElevators();
+            }
+
+            if ( inArea != rangeOfGuide) {
+                deactivateGuide();
             }
 
             if (inArea == elevators2 && Input.GetKey(KeyCode.Alpha1)) {
@@ -132,6 +110,26 @@ public class AreaDetector : MonoBehaviour
                 transform.position = new Vector3(transform.position.x, 2.8f, transform.position.z);
                 activateElevators(1);
             }
+
+            // if (inArea == rangeOfGuide && Input.GetKey(KeyCode.Alpha2)) {
+            //     transform.position = new Vector3(transform.position.x, 2.8f, transform.position.z);
+            //     activateElevators(1);
+            // }
+    }
+
+   private void activateGuide()
+    {
+        guideBG.enabled = true;
+        guideHeader.enabled = true;
+        guideOptions.enabled = true;
+    }
+
+
+    private void deactivateGuide()
+    {
+        guideBG.enabled = false;
+        guideHeader.enabled = false;
+        guideOptions.enabled = false;
     }
 
     private void activateElevators(int floor) {
@@ -149,7 +147,6 @@ public class AreaDetector : MonoBehaviour
     }
 
     private void deactivateElevators() {
-        inArea = null;
         // areaIndicator.text = "Current area: -";
 
         elevatorOptionsBG.enabled = false;
--- campus-guide (unity)/Assets/Scripts/Pathfinding.cs
+++ campus-guide (unity)/Assets/Scripts/Pathfinding.cs
@@ -6,7 +6,9 @@ using UnityEngine.AI;
 public class Pathfinding : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public Transform destinationTag;
+    public Transform office;
+    public Transform elevators1;
+    public Transform elevators2;
 
     void Start() {
         
@@ -15,8 +17,21 @@ public class Pathfinding : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if (Vector3.Distance(transform.position, agent.destination) < 1f) {
+        //     Debug.Log("Close to agent");
+        // }
+
         if (Input.GetKey(KeyCode.P)) {
-            agent.SetDestination(destinationTag.position);
+            agent.SetDestination(elevators1.position);
         }
+        if (Input.GetKey(KeyCode.O)) {
+            agent.SetDestination(elevators2.position);
+        }
+        if (Input.GetKey(KeyCode.K)) {
+            agent.SetDestination(office.position);
+        }
+
+        //if the distance between the player and the agent is less than 1 meter
+
     }
 }
